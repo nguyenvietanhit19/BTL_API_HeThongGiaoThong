d7999a1dcae2cbbed27b1fe2b527315ecead4b73
Hoang Van Nghia <nghiahoang14022005@gmail.com>
2026-04-14 21:49:22 +0700

    nghia admin 1

--- GiaoThongAnToan_API/app.py
+++ GiaoThongAnToan_API/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from routes.auth import auth_bp
 from routes.upload import upload_bp
 from dotenv import load_dotenv
+from routes.admin import admin_bp
 
 load_dotenv()
 
@@ -9,6 +10,8 @@ app = Flask(__name__)
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(upload_bp, url_prefix='')  # ← thêm dòng này
+
+app.register_blueprint(admin_bp, url_prefix='/admin')
 
 if __name__ == '__main__':
     app.run(debug=True)
